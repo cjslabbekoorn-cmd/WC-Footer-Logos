a33e9d0e0f0e454dfb4c6a7f00bada0d434c5b94
cjslabbekoorn-cmd <cjslabbekoorn@gmail.com>
2026-02-26 14:52:38 +0100

    Add files via upload

--- wc-footer-logos.php
+++ wc-footer-logos.php
@@ -2,8 +2,14 @@
 /**
  * Plugin Name: WC Footer Logos (Woo + Elementor)
  * Description: Toon betaal/verzend/keurmerk logo's via shortcode en Elementor widget, incl. provider-indicator en handmatige logo's.
- * Version: 1.0.3
- * Author: Cees-Jan Slabbekoorn (Positie1)
+ * Requires at least: 6.0
+ * Tested up to: 6.5
+ * Requires PHP: 7.4
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Author URI: https://positie1.nl
+ * Version: 1.4.2
+ * Author: Positie1 / Cees-Jan Slabbekoorn
  * Text Domain: wc-footer-logos
  */
 
@@ -11,10 +17,23 @@ if (!defined('ABSPATH')) exit;
 
 define('WCFL_PATH', plugin_dir_path(__FILE__));
 define('WCFL_URL', plugin_dir_url(__FILE__));
-define('WCFL_VERSION', '1.0.3');
+define('WCFL_VERSION', '1.4.2');
+
+define('WCFL_MIN_WP', '6.0');
+define('WCFL_MIN_PHP', '7.4');
+define('WCFL_MIN_WC', '6.0');
+define('WCFL_MIN_ELEMENTOR', '3.0.0');
+
+require_once __DIR__ . '/includes/requirements.php';
 
 require_once WCFL_PATH . 'includes/class-wcfl-plugin.php';
 
 add_action('plugins_loaded', function () {
     WCFL_Plugin::instance();
 });
+
+
+// Requirements & bootstrap
+add_action('plugins_loaded', 'wcfl_requirements_bootstrap', 1);
+
+register_activation_hook(__FILE__, 'wcfl_activation_check');
